e8a39b28a73072d6b34f10ef38597b6ac8b1064c
d0rbu <henryandrecastillo@gmail.com>
2025-09-19 20:10:55 -0500

    shrink seqlen

--- train_gpt_medium.py
+++ train_gpt_medium.py
@@ -529,8 +529,8 @@ class Hyperparameters:
         "data/fineweb10B/fineweb_val_*.bin"  # input .bin to eval validation loss on
     )
     val_tokens = 10485760  # how many tokens of validation data? it's important to keep this fixed for consistent comparisons
-    train_seq_len = 64 * 1024  # FlexAttention sequence length
-    val_seq_len = 4 * 64 * 1024  # FlexAttention sequence length for validation
+    train_seq_len = 512  # FlexAttention sequence length
+    val_seq_len = 512  # FlexAttention sequence length for validation
     # optimization
     num_iterations = 5960  # number of iterations to run
     cooldown_frac = 0.7  # fraction of training spent cooling down the learning rate
