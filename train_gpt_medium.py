b24d9d68abbbf2f7b9f2853af806fec6e2c23aec
d0rbu <henryandrecastillo@gmail.com>
2025-10-04 14:15:08 -0500

    tweak hparams

--- train_gpt_medium.py
+++ train_gpt_medium.py
@@ -531,14 +531,14 @@ class Hyperparameters:
     val_files = (
         "data/fineweb10B/fineweb_val_*.bin"  # input .bin to eval validation loss on
     )
-    train_seq_len = 42 * 1024  # FlexAttention sequence length
+    train_seq_len = 64 * 1024  # FlexAttention sequence length
     val_tokens = (
-        64 * 4 * 42 * 1024
+        40 * 4 * 64 * 1024
     )  # how many tokens of validation data? it's important to keep this fixed for consistent comparisons
-    val_seq_len = 4 * 42 * 1024  # FlexAttention sequence length for validation
+    val_seq_len = 4 * 64 * 1024  # FlexAttention sequence length for validation
     # optimization
-    grad_accum_steps = 4
-    num_iterations = 14_336  # number of iterations to run
+    grad_accum_steps = 8
+    num_iterations = 5960  # number of iterations to run
     cooldown_frac = 0.7  # fraction of training spent cooling down the learning rate
     # architecture
     vocab_size = 50257
